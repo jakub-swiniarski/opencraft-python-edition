e02d0c155c535d63940514a8852ac6a67fedb21f
jakub-swiniarski <jakub-swiniarski@protonmail.com>
2023-07-18 17:39:21 +0200

    window setup

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from ursina import *
-from Block import Block
 from Dirt import Dirt
 
 app = Ursina()
+
+window.title="Minecraft Python Edition"
+window.borderless = False
+window.fullscreen = False
+window.exit_button.visible = False
+#window.fps_counter.enabled = False
 
 dirt=Dirt()
 
