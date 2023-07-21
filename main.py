648aa69e5a9406e79976dc56ba4b51d2bb563a3b
Jakub Świniarski <jakub.swiniarski2007@gmail.com>
2023-07-21 16:21:03 +0200

    dirt can now be placed

--- main.py
+++ main.py
@@ -3,40 +3,44 @@ from blocks.Dirt import Dirt
 from entities.Player import Player
 from blocks.Stone import Stone
 
+
+app = Ursina()
+
+window.title="OpenCraft Python Edition"
+window.borderless = False
+window.fullscreen = True
+window.exit_button.visible = False
+#window.fps_counter.enabled = False
+
+player=Player()
+
+Sky(color=rgb(200,255,255))
+
+dirt=[]
+stone=[]
+
 def input(key):
         #mouse
         if key == 'left mouse down' and mouse.hovered_entity:
             destroy(mouse.hovered_entity)
 
-def main():
-    app = Ursina()
+        if key == 'right mouse down':
+            hit_info = raycast(camera.world_position, camera.forward, distance=5)
+            if hit_info.hit:
+                position=hit_info.entity.position + hit_info.normal
+                newDirt=Dirt(position.x, position.y, position.z)
+                dirt.append(newDirt)
 
-    window.title="OpenCraft Python Edition"
-    window.borderless = False
-    window.fullscreen = True
-    window.exit_button.visible = False
-    #window.fps_counter.enabled = False
+for i in range(21):
+    for j in range(21):
+        newDirt=Dirt(i-10,0,j-10)
+        dirt.append(newDirt)
 
-    player=Player()
+#for mouse sensitivity testing
+#test on the main monitor, jump before testing, wait for fps to stabilize
+newStone=Stone(10,1,0)
+stone.append(newStone)
+newStone=Stone(10,2,0)
+stone.append(newStone)
 
-    Sky(color=rgb(200,255,255))
-
-    dirt=[]
-    stone=[]
-
-    for i in range(21):
-        for j in range(21):
-            newDirt=Dirt(i-10,0,j-10)
-            dirt.append(newDirt)
-
-    #for mouse sensitivity testing
-    #test on the main monitor, jump before testing, wait for fps to stabilize
-    newStone=Stone(10,1,0)
-    stone.append(newStone)
-    newStone=Stone(10,2,0)
-    stone.append(newStone)
-
-    app.run()
-
-if __name__ == "__main__":
-    main()
+app.run()
