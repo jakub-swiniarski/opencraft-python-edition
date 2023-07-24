dd7d4ab53bff9c05843014d86b58fb0f279a187c
Jakub Świniarski <jakub.swiniarski2007@gmail.com>
2023-07-24 16:53:33 +0200

    failed to add triplanar shaders for gold

--- blocks/Gold.py
+++ blocks/Gold.py
@@ -1,8 +1,6 @@
 from blocks.Block import Block
-#from ursina.shaders import triplanar_shader
 
 class Gold(Block):
     def __init__(self,x,y,z):
         super().__init__(x,y,z)
         self.texture='assets/blocks/gold.png'
-        #self.shader=triplanar_shader
