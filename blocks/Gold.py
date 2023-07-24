351a7418de8249ed94c6edb08c1e6ff4c610a045
Jakub Świniarski <jakub.swiniarski2007@gmail.com>
2023-07-24 16:57:02 +0200

    realistic (but laggy) gold

--- blocks/Gold.py
+++ blocks/Gold.py
@@ -1,6 +1,9 @@
 from blocks.Block import Block
+from ursina.shaders import *
 
 class Gold(Block):
     def __init__(self,x,y,z):
         super().__init__(x,y,z)
         self.texture='assets/blocks/gold.png'
+
+        self.shader=matcap_shader
